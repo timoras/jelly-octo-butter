932d9d440010fa6836fb3fa6a24917e0cd02d654
tomasz <tomasz@noemail.com>
2014-06-20 11:47:20 +0300

    math

--- little/scheme_atoms.rkt
+++ little/scheme_atoms.rkt
@@ -17,6 +17,6 @@
       ((null? lat) false)
       (else (or (eq? a (car lat)) (member? a (cdr lat)))))))
 
-(define inc1 
-  (lambda (x)
-    (+ x 1)))
+(define inc1 (lambda (x) (+ x 1)))
+(define add1 (lambda (x) (+ x 1)))
+(define sub1 (lambda (x) (- x 1)))
